264b7dc98287b30ee96d1473fc575b2401d8a2ab
xoz0359 <k84890690@gmail.com>
2025-05-15 15:20:00 +0900

    init

---
+++ src/main/java/co/min/stock_simulator/StockSimulatorApplication.java
@@ -0,0 +1,13 @@
+package co.min.stock_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/min/stock_simulator/StockSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.min.stock_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
